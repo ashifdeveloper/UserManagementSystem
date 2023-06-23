2934890fead3c61e4d8e787089011b9fe70c16bb
Mohammad Ashif <mohdashif995@gmail.com>
2023-06-23 22:17:21 +0530

    Added UserManagementSystem Project

---
+++ src/main/java/com/geekster/UserManagementSystemApp/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.geekster.UserManagementSystemApp.controller;
+
+import com.geekster.UserManagementSystemApp.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+public class UserController {
+    private List<User> userInfo;
+
+    public UserController() {
+        userInfo = new ArrayList<>();
+    }
+
+
+    @PostMapping("addUser")
+    public String addUser(@RequestBody User user) {
+        userInfo.add(user);
+        return "user Added";
+    }
+
+    @GetMapping("getAllUser")
+    public List<User> getAllUser() {
+        return userInfo;
+    }
+
+
+    @GetMapping("getUser/{userId}")
+    public List<User> getUser(@PathVariable Integer userId) {
+        List<User> userIdInfo = new ArrayList<>();
+
+
+        for (User info : userInfo) {
+
+            if (info.getUserId().equals(userId)) {
+                userIdInfo.add(info);
+
+
+            }
+        }
+        return userIdInfo;
+
+    }
+    @PutMapping("updateUserInfo/{userId}")
+    public String updateUserInfo(@PathVariable Integer userId) {
+        for (User info : userInfo) {
+            if (info.getUserId().equals(userId)) {
+                info.setName(info.getName());
+                info.setUserName(info.getUserName());
+                info.setAddress(info.getAddress());
+                info.setPhoneNumber(info.getPhoneNumber());
+                return "User updated for UserId" + userId;
+            }
+        }
+        return "User not found userId:" + userId;
+    }
+    @DeleteMapping("deleteUser/{userId}")
+    public String deleteUser(@PathVariable Integer userId){
+        for(User user : userInfo){
+            if(user.getUserId().equals(userId)){
+                userInfo.remove(user);
+                return "User deleted for userId" + userId;
+            }
+        }
+        return "user:" + userId + "not deleted as it does not exist";
+    }
+
+    }
+
+
---
+++ src/main/java/com/geekster/UserManagementSystemApp/entity/User.java
@@ -0,0 +1,50 @@
+package com.geekster.UserManagementSystemApp.entity;
+
+public class User {
+    private Integer userId;
+    private String name;
+    private String userName;
+    private String address;
+
+    private String phoneNumber;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
